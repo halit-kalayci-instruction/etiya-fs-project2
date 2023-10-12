552ce064a481fbc8e3d80505b0b18cc2d8cc88aa
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-10-12 09:43:49 +0300

    example post request

--- server/index.js
+++ server/index.js
@@ -5,9 +5,21 @@ const app = express();
 
 const server = http.createServer(app);
 
+// req => header,body
 app.get("/", (req, res) => {
+  console.log(req.params);
+  console.log(req.headers);
+  console.log(req.body);
   let array = [{ id: 1, name: 'halit' }, { id: 2, name: 'enes' }]
   res.send(array)
+})
+
+app.post('/', (req, res) => {
+  console.log("Params", req.query);
+  console.log("Headers", req.headers);
+  console.log("Body", req.body);
+  let array = [{ id: 1, name: 'halit' }, { id: 2, name: 'enes' }]
+  res.json(array);
 })
 
 server.listen(9000, () => {
